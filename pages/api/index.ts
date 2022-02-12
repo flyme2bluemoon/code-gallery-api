55551355a5adc181871554dc7d73dfbd2632a1fe
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-02-12 16:48:54 -0500

    created api

--- pages/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default (request: NextApiRequest,response: NextApiResponse) => {
-    response.status(200).json({
-        message: "hello, world!"
-    });
-}
