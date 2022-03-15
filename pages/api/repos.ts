e18a1bcdce690d464ded2982e624c0b37bd4414a
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2022-03-15 11:40:37 -0400

    added languages

--- pages/api/repos.ts
+++ pages/api/repos.ts
@@ -1,11 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
-    const api_res = await fetch("https://api.github.com/users/flyme2bluemoon/repos", {
-        method: "GET",
-        headers: {
-            "Authorization": `token ${process.env.GH_API_KEY}`
-        }
-    }).then(data => data.json());
+    let api_res = undefined;
+    const headers = {
+        "Authorization": `token ${process.env.GH_API_KEY}`
+    }
+
+    if (request.query.name) {
+        api_res = await fetch("https://api.github.com/repos/flyme2bluemoon/Advent-of-Code-2021/languages", {
+            method: "GET",
+            headers: headers
+        }).then(data => data.json());
+    } else {
+        api_res = await fetch("https://api.github.com/users/flyme2bluemoon/repos", {
+            method: "GET",
+            headers: headers
+        }).then(data => data.json());
+    }
+    
     response.status(200).json(api_res);
 }
